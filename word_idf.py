bcb23240ef6ecd25daa64d6f0448c84dc5a007db
Akshansh Gupta <akshedu@gmail.com>
2018-09-03 22:40:03 +0530

    Fix word_idf calculations

--- word_idf.py
+++ word_idf.py
@@ -2,12 +2,19 @@ import re
 from mrcc import CCJob
 from bs4 import BeautifulSoup
 from collections import Counter
-
+import math
+from mrjob.step import MRStep
 
 class WordIDF(CCJob):
     """
- 	Returns list of unique words in a document
+ 	Returns IDF of words in a corpus of WARC documents
     """
+    def steps(self):
+        return [MRStep(mapper=self.mapper,
+                        combiner=self.combiner,
+                        reducer=self.word_frequency),
+                MRStep(mapper_init=self.idf_mapper_init,
+                        mapper=self.idf_mapper)]
 
     def clean_text(self, html):
         soup = BeautifulSoup(html) # create a new bs4 object from the html data loaded
@@ -31,12 +38,34 @@ class WordIDF(CCJob):
         	text = self.clean_text(re.compile('Content-Length: \d+').split(record.payload)[1])
         	alnum_text = re.sub('[^A-Za-z0-9 ]+', ' ', text)
         	for word, counter in Counter(alnum_text.encode('utf-8').lower().split()).iteritems():
-        		yield (record['WARC-Trec-iD'], word), 1
+        		yield word, 1
         except:
         	yield '(an error occurred)', 1
         	return
 
-#    def reducer(self, key, values):
+    def combiner(self, key, values):
+        """
+        Sums up count for each mapper
+        """
+        yield key, sum(values)
+
+    def word_frequency(self, key, values):
+        """
+        Sums up count for each key
+        """
+        yield key, sum(values)
+
+    def idf_mapper_init(self):
+        from myconfig import TOTAL_DOCUMENTS
+        self.total_documents = TOTAL_DOCUMENTS
+    
+    def idf_mapper(self, key, value):
+        """
+        Ouputs IDF of each word using number of documents
+        as a constant
+        """
+        yield key, math.log(self.total_documents / value)
+
 
 if __name__ == '__main__':
     WordIDF.run()
