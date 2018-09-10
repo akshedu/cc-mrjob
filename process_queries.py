672f2fc2f457943ed3c91059832855c5786cfd53
Akshansh Gupta <akshedu@gmail.com>
2018-09-10 13:30:32 +0530

    Fix errors

--- process_queries.py
+++ process_queries.py
@@ -1,24 +1,30 @@
 import re
 import math
 from collections import Counter
+import logging
 
 import psycopg2 as psg
+from psycopg2 import sql
 from mrcc import CCJob
 from bs4 import BeautifulSoup
+from mrjob.util import log_to_stream
 
 from sql_queries import DOCUMENT_CANONICAL_TYPES
 from sql_queries import FILTER_QUERIES
 from sql_queries import DOCUMENT_ENTITY_CANONICAL_TYPES
 from sql_queries import WORD_IDF
 
+LOG = logging.getLogger('ProcessQuery')
+log_to_stream(format="%(asctime)s %(levelname)s %(name)s: %(message)s", name='ProcessQuery')
+
 
 class ProcessQuery(CCJob):
     """
- 	Returns IDF of words in a corpus of WARC documents
+ 	Process batch of queries
     """
     def mapper_init(self):
         self.conn = psg.connect('dbname=qa_app \
-                                user=test_user \
+                                user=akshanshgupta \
                                 password=Everest \
                                 host=127.0.0.1 \
                                 port=5432')
@@ -29,31 +35,37 @@ class ProcessQuery(CCJob):
     def get_doc_canonical_types(self, document_id):
         doc_entity_types_query = self.cur.execute(DOCUMENT_CANONICAL_TYPES.\
                                             format(document_id=document_id))
-        doc_entity_types = doc_entity_types_query.fetchall()
+        doc_entity_types = self.cur.fetchall()
         return tuple([result[1] for result in doc_entity_types])
 
     def get_doc_queries(self, doc_canonical_types):
-        doc_queries_query = self.cur.execute(FILTER_QUERIES.\
-                                        format(types=doc_canonical_types))
-        return doc_queries_query.fetchall()
+        doc_canonical_types = sql.Literal(doc_canonical_types)
+        doc_queries_query = self.cur.execute(sql.SQL(FILTER_QUERIES).\
+                                        format(types=doc_canonical_types).as_string(self.conn))
+        doc_queries = self.cur.fetchall()
+        return doc_queries
 
     def get_idf_score(self, words):
-        idf_scores = self.cur.execute(WORD_IDF.format(words=tuple(words)))
+        words = sql.Literal(tuple(words))
+        idf_scores_query = self.cur.execute(sql.SQL(WORD_IDF).\
+                                        format(words=words).as_string(self.conn))
+        idf_scores = self.cur.fetchall()
         return sum([float(result[1]) for result in idf_scores])
 
     def get_text_snippet_query(self, query_id, S, target_type, document_id,
                                 doc_entity_canonical_types, doc_html_text):
         S = S.split(',')
         for item in doc_entity_canonical_types:
-            if item[8] == target_type:
+            if item[9] == target_type:
                 start_byte = item[3] - self.WINDOW_SIZE
                 end_byte = item[4] + self.WINDOW_SIZE
                 window_text =  doc_html_text[start_byte:end_byte]
                 window_text = re.sub('[^A-Za-z0-9 ]+', ' ', window_text)
                 window_words = window_text.lower().split()
-                matching_words =  list(set(window_words) & set(S))
-                total_score = get_idf_score(matching_words)
-                yield query_id, (document_id, window_text, total_score)
+                matching_words = list(set(window_words) & set(S))
+                if not matching_words == []:
+                    total_score = self.get_idf_score(matching_words)
+                    yield query_id, (document_id, window_text, total_score)
 
 
     def clean_text(self, html):
@@ -76,19 +88,28 @@ class ProcessQuery(CCJob):
             return
         try:
             document_id = record['WARC-TREC-ID']
-            doc_canonical_types = get_doc_canonical_types(document_id)
+            doc_canonical_types = self.get_doc_canonical_types(document_id)
             if not doc_canonical_types:
                 return
-            relevant_queries = get_doc_queries(doc_canonical_types)
+            relevant_queries = self.get_doc_queries(doc_canonical_types)
             if not relevant_queries:
                 return
-            doc_entity_canonical_types = self.cur.execute(DOCUMENT_ENTITY_CANONICAL_TYPES.\
-                                                    format(document_id=document_id))
+            doc_entity_canonical_types_query = self.cur.execute(DOCUMENT_ENTITY_CANONICAL_TYPES.\
+                                                                    format(document_id=document_id))
+            doc_entity_canonical_types = self.cur.fetchall()
             doc_encoding = doc_entity_canonical_types[0][1]
-            doc_html_text = record.payload.decode(doc_encoding).encode('utf-8')
+            
+            if doc_encoding != 'UTF-8':
+                try:
+                    doc_html_text = record.payload.decode(doc_encoding).encode('utf-8')
+                except:
+                    LOG.info('Could not decode document %s', document_id)
+                    return   
+            else:
+                doc_html_text = record.payload
 
-            for _, S, target_type in relevant_queries:
-                for key, value in get_text_snippet_query(query_id, S, target_type, document_id,
+            for query_id, _, S, target_type in relevant_queries:
+                for key, value in self.get_text_snippet_query(query_id, S, target_type, document_id,
                                                         doc_entity_canonical_types, doc_html_text):
                     yield key, value
         except:
@@ -102,14 +123,16 @@ class ProcessQuery(CCJob):
         """
         Sums up count for each mapper
         """
-        yield key, [val for val in values if val[2] > self.MIN_SCORE]
+        for val in values:
+            if val[2] > self.MIN_SCORE:
+                yield key, val
     
     def reducer(self, key, values):
         """
         Ouputs IDF of each word using number of documents
         as a constant
         """
-        values = sorted(values, key=lambda tup: tup[2], reverse=True)
+        values = sorted(values, key=lambda x: x[2], reverse=True)
         for val in values:
             yield (key, val[0]), (val[1], val[2])
 
